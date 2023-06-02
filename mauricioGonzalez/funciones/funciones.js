d8acc4ccc71d0592f252d9fbca7055779fe562ff
Mauricio Gonzalez Morales <mauriciogonzalez325@hotmail.com>
2023-06-01 23:31:56 -0600

    Mis primeras funciones

---
+++ mauricioGonzalez/funciones/funciones.js
@@ -0,0 +1,36 @@
+function miPrimeraFuncion(){
+    console.log("Hello World");
+}
+
+
+function suma(a,b){
+    console.log("El resultado de la suma de "+a+"+"+b+" ="+(a+b));
+
+}
+
+function resta(a,b){
+    console.log("El resultado de la resta de "+a+"-"+b+" ="+(a-b));
+
+}
+
+function division(a,b){
+    console.log("El resultado de la division de "+a+"/"+b+" ="+(a/b));
+
+}
+
+function multiplicacion(a,b){
+    console.log("El resultado de la multiplicacion de "+a+"*"+b+" ="+(a*b));
+
+}
+
+/* tener en cuenta de cuantos parametros estoy recibiendo y de que tipo,
+como tambien siempre llamarla con sus respectivos parametros */
+
+
+suma(5,5);
+resta(10,3);
+division(20,2);
+multiplicacion(3,3);
+
+
+miPrimeraFuncion();
